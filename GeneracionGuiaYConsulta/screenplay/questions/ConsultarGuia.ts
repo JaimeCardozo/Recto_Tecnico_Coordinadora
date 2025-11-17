db08847105996add9b543a5de14a1c251c9df08e
Jaime Cardozo <sqa20@red5g.co>
2025-11-17 15:25:43 -0500

    MejoraReporteAllure

--- GeneracionGuiaYConsulta/screenplay/questions/ConsultarGuia.ts
+++ GeneracionGuiaYConsulta/screenplay/questions/ConsultarGuia.ts
@@ -1,6 +1,6 @@
 import { Actor } from '../actors/Actor';
 import { CallApiWithToken } from '../abilities/CallApiWithToken';
-import { expect } from '@playwright/test';
+import test, { expect } from '@playwright/test';
 
 export class ConsultarGuia {
   static conId(id: string, datosEsperados: Partial<any>) {
@@ -16,7 +16,10 @@ export class ConsultarGuia {
     const api = (actor.abilityTo(CallApiWithToken) as CallApiWithToken).context();
 
     const response = await api.get(`/guias/${this.id}`);
-    expect(response.status()).toBe(200);
+    await test.step('Se valida que el estado de respuesta del GET sea 200 OK', async () => {
+      expect(response.status()).toBe(200);
+    })
+    
 
     const body = await response.json();
     const detalle = body.data.detalle;
@@ -25,8 +28,16 @@ export class ConsultarGuia {
     console.log('📦 Respuesta del GET /guias/{id}:', JSON.stringify(body, null, 2));
 
     // Validaciones de integridad
-    expect(body.data.referenciaRecaudo).toBe(this.esperado.referenciaRecaudo);
-    expect(body.data.valorRecaudar).toBe(Number(this.esperado.valorRecaudar));
+    await test.step('Se valida que el campo referenciaRecaudo tenga el mismo valor que se envio', async () => {
+      expect(body.data.referenciaRecaudo).toBe(this.esperado.referenciaRecaudo);
+    })
+
+    await test.step('Se valida que el campo valorRecuadar tenga el mismo valor que se envio', async () => {
+      expect(body.data.valorRecaudar).toBe(Number(this.esperado.valorRecaudar));
+    })
+    
+    
+    
     
     console.log('✅ Guía validada correctamente');
   }
